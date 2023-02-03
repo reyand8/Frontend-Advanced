839cddb9f3f05af354e25109dab2598db847f69a
Andrey <andreeey.gg@gmail.com>
2023-02-03 17:39:32 +0100

    Main

---
+++ HW-2/index.js
@@ -0,0 +1,23 @@
+const operator = (prompt('Select an operator: *, +, -, /'))
+const first_number = Number(prompt('Enter operand A'))
+const second_number = Number(prompt('Enter operand B'))
+
+
+switch (operator) {
+    case '*':
+        result = first_number * second_number;
+        break;
+    case '+':
+        result = first_number + second_number ;
+        break;
+    case '-':
+        result = first_number - second_number;
+        break;
+    case '/':
+        result = first_number / second_number;
+        break;
+    default:
+        alert('Please choose the correct operator')
+}
+
+alert(String(`${first_number} ${operator} ${second_number} = ${result}`));
