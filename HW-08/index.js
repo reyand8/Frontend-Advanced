27ad670535c6d2c9ae8e20578ff996b84dfb7ae6
Andrey <andreeey.gg@gmail.com>
2023-02-24 18:43:57 +0100

    main

---
+++ HW-08/index.js
@@ -0,0 +1,34 @@
+'use strict'
+function createCalculator(base = 0) {
+    let result = base
+
+    return {
+        add: (n) => { (typeof n === 'number') ? result += n : NaN},
+        sub: (n) => { (typeof n === 'number') ? result -= n : NaN},
+        set: (n) => {(typeof n === 'number') ? result = n : NaN},
+        reset: () => {result = base},
+        get: () => {
+            return result
+        },
+    }
+}
+
+
+const calculator = createCalculator(100);
+
+
+calculator.add(10);
+calculator.add(10);
+calculator.sub(20);
+
+
+calculator.set(20);
+console.log(calculator.get())
+calculator.add(10);
+calculator.add(10);
+calculator.add('qwe');
+
+
+console.log(calculator.get())
+calculator.reset();
+console.log(calculator.get())
