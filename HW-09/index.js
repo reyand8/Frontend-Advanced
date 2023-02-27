2b6df3b514e5385dfaff24d5eac137c3446751aa
Andrey <andreeey.gg@gmail.com>
2023-02-27 20:24:29 +0100

    main

---
+++ HW-09/index.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const calculator = new CreateCalculator(0);
+
+calculator.add(10);
+calculator.add(10);
+calculator.sub(20);
+
+
+calculator.set(20);
+console.log(calculator.get())
+calculator.add(10);
+calculator.add(10);
+calculator.add('qwe');
+
+console.log(calculator.get())
+calculator.reset();
+console.log(calculator.get())
+
+
+function CreateCalculator(base) {
+    this.result = base
+    this.add = function (n) {
+        if (isNumber(n)) {
+            this.result += n
+        }
+    }
+    this.sub = function (n) {
+        if (isNumber(n)) {
+            this.result -= n
+        }
+    }
+    this.set = function (n) {
+        if (isNumber(n)) {
+            this.result = n
+        }
+    }
+    this.reset = function () {
+        this.result = base
+    }
+    this.get = function () {
+        return this.result
+    }
+}
+
+
+function isNumber (n) {
+    return typeof n === 'number'
+}
