68f00e6e33ed73cbfa23fe3345f7e7b03978e566
Andrey <andreeey.gg@gmail.com>
2023-03-12 10:56:08 +0100

    add two functions

--- HW-12/index.js
+++ HW-12/index.js
@@ -6,12 +6,21 @@ const ul = document.querySelector('#todoList')
 
 btn.addEventListener('click', onBtnClick)
 
-
 function onBtnClick () {
-    if (input.value !== '' ) {
-        const li = document.createElement('li')
-        li.textContent = input.value
-        ul.append(li)
+    const data = getInputData()
+    if (!isInputValid(data)) {
+        return alert('Invalid data')
     }
+    const li = document.createElement('li')
+    li.textContent = input.value
+    ul.append(li)
     input.value = ''
+}
+
+function getInputData () {
+    return input.value
+}
+
+function isInputValid (data) {
+    return data !== ''
 }
